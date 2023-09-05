5366c7907d6d62f6de9659c8b3ae9b5ae1a87dbf
Neeraja M <87661670+neeraja-m@users.noreply.github.com>
2023-09-05 18:06:40 +0400

    Delete extension directory

--- extension/SortSpeeds.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortSpeeds implements Comparator<Character> {
-
-  public int compare(Character a, Character b){ //Sorts speed of characters according to speed in descending order
-    return b.getSpeed() - a.getSpeed();
-  }
-}
